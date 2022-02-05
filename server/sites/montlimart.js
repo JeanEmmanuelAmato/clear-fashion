a27e13fcddc53167ee3ea7c7fa7765c811f4cd11
JeanEmmanuelAmato <jean.emmanuel9999@gmail.com>
2022-02-05 10:03:45 +0100

    parse data from montlimart

--- server/sites/montlimart.js
+++ server/sites/montlimart.js
@@ -27,5 +27,29 @@ const parse = (data) => {
                 
         };
     }).get();
-
 };
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+
+ module.exports.scrape = async url => {
+    try {
+      const response = await fetch(url);
+  
+      if (response.ok) {
+        const body = await response.text();
+  
+        return parse(body);
+      }
+  
+      console.error(response);
+  
+      return null;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
