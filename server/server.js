4abef92d3909e937c7b6ed399aad20600ec7cbb4
JeanEmmanuelAmato <jean.emmanuel9999@gmail.com>
2022-02-14 19:52:36 +0100

    insert data

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const {MongoClient} = require('mongodb');
 const MONGODB_URI = 'mongodb+srv://AmatoJeanEmmanuel:clearfashion@clearfashion.yjbvj.mongodb.net/ClearFashion?retryWrites=true&w=majority';
 const MONGODB_DB_NAME = 'clearfashion';
-const products = require('products_for_all_brands');
+const products = require('./products_for_all_brands.json');
 let db, collection;
 
 const connect = async () => {
@@ -9,11 +9,23 @@ const connect = async () => {
         const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
         db =  client.db(MONGODB_DB_NAME);
         console.log(`Connected to ${MONGODB_DB_NAME}...`);
-        console.log(db);
+        // console.log(db);
     }catch(e){
         console.error(e);
     } 
 }
 
-//connect()
+const insertProduct = async () => {
+    try {
+        await connect();
+        collection = db.collection('products');
+        const result = collection.insertMany(products);
+        console.log(result);
+    }
+    catch(e){
+        console.error(e);
+    }
+}
+
+insertProduct()
 
